b46fc03864b1b82e74fd4f4d878d69ec3a4f97d8
Will McAuliff <womcauliff@gmail.com>
2016-07-23 23:06:02 -0400

    Added countdown timer object, sets interval on page reload, updates screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,32 @@
-$(document).ready(function () {
-	
-});
+
+
+window.onload = function () {
+	countdownTimer.reset();
+	countdownTimer.start();
+};
+
+var countdownTimer = {
+		time: 0,
+		reset: function() {
+			console.log("reset");
+			countdownTimer.time = 10;
+			$("#time-left").html(countdownTimer.time);
+		},
+		start: function() {
+			console.log("start");
+			intervalID = setInterval(countdownTimer.tick, 1000);
+		},
+		stop: function() {
+			console.log("stop");
+			clearInterval(intervalID);
+		},
+		tick: function() {
+			console.log("tick");
+			countdownTimer.time--;
+			$("#time-left").html(countdownTimer.time);
+
+			if(countdownTimer.time === 0) {
+				countdownTimer.stop();
+			}
+		},
+	}
