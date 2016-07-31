d4e53f704e07d09f157bc4bee8046eebb26e7b5b
Will McAuliff <womcauliff@gmail.com>
2016-07-30 20:22:56 -0400

    QuizQuestion constructor created. Trivia Game Object created, controling timer and question cycling.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,103 @@
+/**
+ * QuizQuestion Constructor
+ */
+function QuizQuestion(
+					questionText,
+					answers,
+					solutionIndex
+					) {
+	var q = {
+		questionText: questionText,
+		answers: answers,
+		solutionIndex: solutionIndex
+	};
+	return q;
+}
 
+/**
+ * Trivia Game Object
+ *
+ * Controls the StopWatch for the game,
+ * as well as the Quiz Question and Answer Logic.
+ */
+var triviaGame = {
 
-window.onload = function () {
-	countdownTimer.reset();
-	countdownTimer.start();
-};
-
-var countdownTimer = {
-		time: 0,
+		//StopWatch Section
+		defaultTime: 0,
+		currentTime: 0,
 		reset: function() {
 			console.log("reset");
-			countdownTimer.time = 10;
-			$("#time-left").html(countdownTimer.time);
+			triviaGame.currentTime = triviaGame.defaultTime;
+			$("#time-left").html(triviaGame.currentTime);
+			console.log("count: " + triviaGame.count);
+			var currentQuizQuestion = triviaGame.quiz[triviaGame.count];
+			$("#question").html(currentQuizQuestion.questionText);
+			for (var i = 0; i < currentQuizQuestion.answers.length; i++) {
+				$("#answers").append(
+					$('<div>')
+						.addClass("answer")
+						.text(currentQuizQuestion.answers[i])
+				);
+			};
 		},
 		start: function() {
 			console.log("start");
-			intervalID = setInterval(countdownTimer.tick, 1000);
+			intervalID = setInterval(triviaGame.tick, 1000);
 		},
 		stop: function() {
 			console.log("stop");
 			clearInterval(intervalID);
+			$("#question").html("");
+			$("#answers").empty();
 		},
 		tick: function() {
 			console.log("tick");
-			countdownTimer.time--;
-			$("#time-left").html(countdownTimer.time);
+			triviaGame.currentTime--;
+			$("#time-left").html(triviaGame.currentTime);
 
-			if(countdownTimer.time === 0) {
-				countdownTimer.stop();
+			//Ran out of time for question
+			if(triviaGame.currentTime === 0) {
+				console.log("Time's Up.");
+				triviaGame.stop();
+				triviaGame.count++;
+				if(triviaGame.count < triviaGame.quiz.length) {
+					triviaGame.reset();
+					triviaGame.start();
+				}
+				else {
+					console.log("no more questions");
+				}
 			}
 		},
+
+		//Quiz Section
+		count: 0,// the index of the currently displaying question.
+		quiz: [],
+		addQuestion: function(question) {
+			console.log("added " + question.questionText + " to " + this.quiz.length);
+			this.quiz[this.quiz.length] = question;
+		},
 	}
+
+var intervalID;
+
+function setUp() {
+	triviaGame.defaultTime = 3;
+	triviaGame.addQuestion(new QuizQuestion("Why?", 
+						["because", "why not", "nunya beezwax", "i said so"],
+						0
+						));
+	triviaGame.addQuestion(new QuizQuestion("Where?",
+						["here", "there", "hither", "thither"],
+						2
+						));
+	triviaGame.addQuestion(new QuizQuestion("How?",
+						["try harder", "cheat", "fake it", "guess"],
+						3));
+	triviaGame.reset();
+}
+
+window.onload = function () {
+	setUp();
+	triviaGame.start();
+};
